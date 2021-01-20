b39cd4b55254c2ca8975f01f342a5332cbb03c4c
LauraToddCode <laura20todd@gmail.com>
2021-01-20 10:09:11 +0000

    Adding files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Slide Toggle
  Slide toggle with rolling ball
+ View on Codepen - https://codepen.io/lauratodddesign/pen/GRjPBZZ
---
+++ index.js
@@ -0,0 +1,12 @@
+const ball = document.getElementById("ball");
+
+ball.addEventListener("click", function () {
+  if (ball.classList.contains("moveRight")) {
+    ball.classList.add("moveLeft");
+    ball.classList.remove("moveRight")
+  } else {
+    ball.classList.add("moveRight");
+    ball.classList.remove("moveLeft")
+  }
+  
+});
