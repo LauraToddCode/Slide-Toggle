80c2876f70a477b7e794745e2942fcbfd656299a
LauraToddCode <laura20todd@gmail.com>
2021-02-07 12:03:14 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const ball = document.getElementById("ball");
 ball.addEventListener("click", function () {
   if (ball.classList.contains("moveRight")) {
     ball.classList.add("moveLeft");
-    ball.classList.remove("moveRight")
+    ball.classList.remove("moveRight");
   } else {
     ball.classList.add("moveRight");
-    ball.classList.remove("moveLeft")
+    ball.classList.remove("moveLeft");
   }
   
 });
